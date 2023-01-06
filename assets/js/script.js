a6806a8f9c3193a62580b4ab4e83d664cf87f571
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2023-01-06 22:55:47 +0600

    Project Upload

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// Function//
+const calculateButton = document.getElementById("calculate-button");
+
+calculateButton.addEventListener("click", function () {
+  // Function variable
+  const income = document.getElementById("income").innerText;
+  const rentInput = document.getElementById("rent");
+  const foodInput = document.getElementById("food");
+  const utilitiesInput = document.getElementById("utilities");
+  const insuranceInput = document.getElementById("insurance");
+  const expensesDiv = document.getElementById("expenses");
+  const balanceDiv = document.getElementById("balance");
+  const errorText = document.getElementById("error-text");
+
+  //Variable value
+  const incomeValue = parseFloat(income);
+  const rentValue = parseFloat(rentInput.value); // We use "parseFloat()" function to convert input field 'srting' value to 'number'
+
+  const foodValue = parseFloat(foodInput.value);
+  const utilitiesValue = parseFloat(utilitiesInput.value);
+  const insuranceValue = parseFloat(insuranceInput.value);
+
+  if (rentValue && foodValue && utilitiesValue && insuranceValue) {
+    console.log(rentValue);
+    errorText.innerHTML = "";
+    rentInput.value = "";
+    foodInput.value = "";
+    utilitiesInput.value = "";
+    insuranceInput.value = "";
+  } else {
+    errorText.innerHTML = `<p>Please enter a valid number to Caculate</p>`;
+  }
+  // Calculation of total cost as "cost" and remaining balance as "balance"
+  if (rentValue && foodValue && utilitiesValue && insuranceValue >= 0) {
+    const cost = rentValue + foodValue + utilitiesValue + insuranceValue;
+    expensesDiv.innerText = cost;
+    const balance = incomeValue - cost;
+    balanceDiv.innerText = balance;
+  }
+});
